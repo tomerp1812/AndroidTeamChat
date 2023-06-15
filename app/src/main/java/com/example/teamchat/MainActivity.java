4f30e919b8dc7d55c0c041fc545733c68740f712
IditMedizada <iditm9@gmail.com>
2023-06-15 16:29:47 +0300

    MainActivity

--- app/src/main/java/com/example/teamchat/MainActivity.java
+++ app/src/main/java/com/example/teamchat/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.teamchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,7 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.teamchat.api.userApi;
+import com.example.teamchat.chats.ContactList;
 
 public class MainActivity extends AppCompatActivity {
     public static Context context;
